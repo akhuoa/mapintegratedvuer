5ab6403a7719fe225dcf98f7a5fae91ba83997c3
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-02-19 12:29:00 +1300

    Preview allowHost for demo

--- vite.config.js
+++ vite.config.js
@@ -49,5 +49,9 @@ export default defineConfig(({ command, mode }) => {
       // 'process.env': env
     };
   }
+
+  if (command === 'preview') {
+    config.preview.allowedHosts = 'mapintegratedvuer-mapcore-754fcb3bf891.herokuapp.com'
+  }
   return config;
 });
