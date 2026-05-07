4b865a81919c389675186b32fcc2bb80b780505e
alan-wu <alan.wu@auckland.ac.nz>
2026-05-07 14:31:29 +1200

    Update a test.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -185,6 +185,7 @@ Cypress.Commands.add('connectivitySearch', (searchTerm) => {
   cy.get('[style=""] > .el-card__header > .header > .search-input-container > .el-input > .el-input__wrapper > .el-input__inner').clear();
   cy.get('[style=""] > .el-card__header > .header > .search-input-container > .el-input > .el-input__wrapper > .el-input__inner').type(searchTerm);
   cy.get('[style=""] > .el-card__header > .header > .el-button--primary').click();
+  cy.wait(10000);
   cy.get('.connectivity-card-container > .connectivity-card').should('have.length.greaterThan', 0);
   cy.get('.dataset-results-feedback:visible').should('exist').contains('results');
 });
