14a83b56e0431a4a8893051cb6f0184bb18cb839
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-26 14:47:16 +1200

    Update env variables usage in Cypress testing

--- cypress/component/MapContent.cy.js
+++ cypress/component/MapContent.cy.js
@@ -41,16 +41,19 @@ describe('MapContent', () => {
 
     //cy.intercept('GET', 'https://mapcore-demo.org/current/flatmap/v2/**');
 
-    cy.mount(MapContent, {
-      props: {
-        options: {
-          sparcApi: "https://mock-test/sparc-api/",
-          flatmapAPI: "https://mapcore-demo.org/current/flatmap/v3/",
-          algoliaKey: Cypress.env('ALGOLIA_KEY'),
-          algoliaId: Cypress.env('ALGOLIA_ID'),
-        }
-      },
-    });
+    cy.env(['ALGOLIA_KEY', 'ALGOLIA_ID']).then(({ ALGOLIA_KEY, ALGOLIA_ID }) => {
+      cy.mount(MapContent, {
+        props: {
+          options: {
+            sparcApi: "https://mock-test/sparc-api/",
+            flatmapAPI: "https://mapcore-demo.org/current/flatmap/v3/",
+            algoliaKey: ALGOLIA_KEY,
+            algoliaId: ALGOLIA_ID,
+          }
+        },
+      })
+    })
+
 
     let mockClipboardText = '';
 
